9e8a92d4bdf71fe8a03d2f13ca10880dd72b9f7e
Seth Bowman <brandonsethbowman@gmail.com>
2024-11-05 15:36:04 -0600

    demo completed

---
+++ VariablesDemoCSharp-52/Program.cs
@@ -0,0 +1,47 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        //Variables
+        //camelCase for variable names = everyOtherWordStartsWithCapital
+        
+        //Number types
+        //Declaring a variable (creation of a variable)
+        int wholeNumber;
+        
+        //Initializing a variable (giving a value to a variable)
+        wholeNumber = 5;
+        
+        //Or do both
+        //Declaring and Initialzing
+        int myAge = 30;
+        
+        double average = 99.87;
+
+        decimal myAccount = 5000.00m;
+        
+        //Word types
+
+        string ourClass = "CSharp-52";
+
+        char initial = 'S';
+        
+        //Fact types
+        
+        bool isCoding = true;
+        
+        //String interpolation
+
+        //string message = $"I am {myAge} years old";
+
+        //Console.WriteLine($"I am {myAge} years old and I have {myAccount} in my account.");
+        
+        //Take user input
+        
+        Console.WriteLine("Give me your name:");
+        
+        string userName = Console.ReadLine();
+
+        Console.WriteLine($"Hello, {userName}!");
+    }
+}
